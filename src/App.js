902ae83159e68fec9ad6e502e085f984e900d08b
naginshrestha <72690596+naginshrestha@users.noreply.github.com>
2023-03-18 23:00:45 +1100

    message

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import './App.css';
+import { BrowserRouter ,Routes,Route} from 'react-router-dom';
+import { Login } from './Pages/Login';
+import { Registration } from './Pages/Registration';
+import { ForgetPassword } from './Pages/ForgetPassword';
+import { Dashboard } from './Pages/Dashboard';
+import { Layout } from './Components/Layout';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" >
+      <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path='/' element={<Login/>} />
+          <Route path='/registration' element={<Registration/>} />
+          <Route path='/forgetpassword' element={<ForgetPassword/>} />
+          <Route path='/dashboard' element={<Dashboard/>} />
+
+
+        </Routes>
+        </Layout>
+      </BrowserRouter>
+
 
     </div>
   );
